b3619f00350cd6a4e6ada29e2e23203f7e46e213
Maria Nefulda <147525812+Mariantix@users.noreply.github.com>
2023-11-28 09:29:20 -0700

    update main.js

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 console.log ('Hello, World!');
 
 var footer = document.getElementById("FOOTER");
-var currentYear = newDate().getFullYear();
+var currentYear = newDate().getFullYear(); { 
 document.getElementById("currentYear").innerHTML = currentYear;
-
+}
+function myFunction() {
 document.getElementById("alerting").innerHTML = "Hello World";
+}
