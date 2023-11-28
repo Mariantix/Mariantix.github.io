33e92e32e1037e2a6efc704f694d86fbe2a899da
Maria Nefulda <147525812+Mariantix@users.noreply.github.com>
2023-11-28 09:01:42 -0700

    commit changes

--- main.js
+++ main.js
@@ -2,3 +2,6 @@ console.log ('Hello, World!');
 
 var x = document.getElementById("FOOTER");
 document.getElementById("copyright-year").innerHTML = new Date().getFullYear();
+
+document.getElementById("btn-alert");
+alert("This is an alert")
