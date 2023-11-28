89975de615cc7788acbc47f64aab0bd7146123fc
Maria Nefulda <147525812+Mariantix@users.noreply.github.com>
2023-11-28 09:20:03 -0700

    commit changes

--- main.js
+++ main.js
@@ -3,3 +3,5 @@ console.log ('Hello, World!');
 var footer = document.getElementById("FOOTER");
 var currentYear = newDate().getFullYear();
 document.getElementById("currentYear").innerHTML = currentYear;
+
+document.getElementById("alerting").innerHTML = "Hello World";
